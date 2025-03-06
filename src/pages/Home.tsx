776bb66aa4d9851ba6e977afc87d9bb2028edce7
YvonneJL <yvonneloder@gmx.de>
2025-03-06 11:10:02 +0100

    init Beer API Project

---
+++ src/pages/Home.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import allBeers from "../../public/images/allBeer.png";
+import randomBeer from "../../public/images/RandomBeer.png";
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <section className='home'>
+        <div className='all-beers'>
+            <Link to={"/allbeers"}>
+            <img src="../../public/images/Group4.png" alt="all-beers" />
+            </Link>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime nihil vel in quis distinctio vitae. Aperiam, inventore repellat facilis id dolore modi asperiores, quod obcaecati nostrum hic at. Eius, cumque.</p>
+        </div>
+        <div className='random-beer'>
+            <Link to={"/randombeer"}>
+            <img src="../../public/images/Group5.png"alt="random beer" />
+            </Link>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime nihil vel in quis distinctio vitae. Aperiam, inventore repellat facilis id dolore modi asperiores, quod obcaecati nostrum hic at. Eius, cumque.</p>
+        </div>
+
+    </section>
+  )
+}
+
+export default Home
